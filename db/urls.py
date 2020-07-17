cd317bd964a2473bc084ea066746f1f51357b88a
Luis Gonçalo <lmagoncalo@hotmail.com>
2020-07-17 10:06:00 +0100

    Changed to flask

--- db/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .apiviews import PersonList, PersonDetail, CreatePerson
-
-urlpatterns = [
-    path("db/", PersonList.as_view(), name="db_list"),
-    path("db/<int:pk>/", PersonDetail.as_view(), name="db_detail"),
-    path("db/add/", CreatePerson.as_view(), name="create_person"),
-]
